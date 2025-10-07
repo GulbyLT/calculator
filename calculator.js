5227d45ed940273836ed938a5eb0d5b54a35cf46
GulbyLT <gulbinasrok@gmail.com>
2025-10-07 23:08:22 +0300

    add number and operator buttons, todo: make buttons equally sized

--- calculator.js
+++ calculator.js
@@ -39,3 +39,13 @@ function operate(a, b, op){
             break;
     }
 }
+
+let numberButton = document.getElementById("numberButtons");
+for(let i = 0; i < 10; i++){
+    let button = document.createElement("button");
+    button.textContent = i;
+    button.value = i;
+    button.classList.add("numberButton");
+    numberButton.appendChild(button);
+    button.style.width = "50px";
+}
